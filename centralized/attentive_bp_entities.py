58c40ea9220468db958a95bc23aacc0ecc6b9339
Deng Yanchen <dyc941126@126.com>
2021-12-03 12:35:46 +0800

    home

--- centralized/attentive_bp_entities.py
+++ centralized/attentive_bp_entities.py
@@ -2,6 +2,8 @@ import random
 
 import torch
 
+from entities import VariableNode, FunctionNode
+
 
 def msg_to_tensor(msg, device):
     if type(msg) is list:
@@ -11,16 +13,19 @@ def msg_to_tensor(msg, device):
     return msg.squeeze()
 
 
-class Feature:
+class FeatureConstructor:
     vn_id_embed_ub = 0.5
 
     def __init__(self, variable_nodes, function_nodes, assignment_node_prefix=None, summarize_node_prefix=None,
                  node_embed_dim=8, ass_to_sum_hidden=8, sum_to_ass_hidden=8, device='cpu',
                  ass_to_sum_prefix=None, sum_to_ass_prefix=None):
-        self.vn_node_index = dict()  # {x_i: [START, END) of assignment nodes}
-        self.fn_node_index = dict()  # {f_ij: {x_i: [START, END) of summarize nodes to x_j}}
+        self.vn_node_index = dict()  # {x_i: [START, END) of assignment nodes} in self.x
+        self.fn_node_index = dict()  # {f_ij: {x_i: [START, END) of summarize nodes to x_j}} in self.x
+        self.neighbor_idx_info = dict()  # {x_i: {f_ij.idx: [fik.idx]}}
+        self.neighbor_idx_mapping = dict()  # {x_i: {f_ij.name: f_ij.idx}}
         function_nodes = list(function_nodes)
         self.function_nodes = function_nodes
+
         if assignment_node_prefix is None:
             assignment_node_prefix = [1, 0]
         if summarize_node_prefix is None:
@@ -31,38 +36,53 @@ class Feature:
         x = []
 
         # build init node embedding for assignment nodes
+        scatter_indexes = []  # used for pooling: specifies the group each row of embedding matrix belongs to
         for vn in variable_nodes:
+            self.neighbor_idx_info[vn.name] = dict()
+            self.neighbor_idx_mapping[vn.name] = dict()
             start = len(x)
-            vn_id_embed = [random.uniform(0, Feature.vn_id_embed_ub) for _ in range(vn_id_embed_len)]
+            vn_id_embed = [random.uniform(0, FeatureConstructor.vn_id_embed_ub) for _ in range(vn_id_embed_len)]
             vn_embed = assignment_node_prefix + vn_id_embed
             for _ in range(vn.dom_size):
                 x.append(vn_embed)
+                scatter_indexes.append(0)  # we do not care assignment nodes, just assign 0
             end = len(x)
             self.vn_node_index[vn.name] = (start, end)
 
         # build init node embedding for summarize nodes
+        scatter_dom_size = [1]  # used for pooling
+        idx = 1
         sn_id_embed_len = node_embed_dim - len(summarize_node_prefix)
-        sn_embed = summarize_node_prefix + [0 for _ in range(sn_id_embed_len)]
+        sn_embed = summarize_node_prefix + [0 for _ in range(sn_id_embed_len)]  # 0 for padding
         for fn in function_nodes:
             i = fn.row_vn.name
             j = fn.col_vn.name
             start = len(x)
             for _ in range(fn.col_vn.dom_size):
                 x.append(sn_embed)
+                scatter_indexes.append(idx)  # all f_ij -> x_j summarize nodes belong to the same group
+                scatter_dom_size.append(fn.col_vn.dom_size)
             end = len(x)
+            idx += 1  # increase group id
             self.fn_node_index[fn.name] = {i: (start, end)}
             start = len(x)
             for _ in range(fn.row_vn.dom_size):
                 x.append(sn_embed)
+                scatter_indexes.append(idx)  # all f_ij -> x_i summarize nodes belong to the same group
+                scatter_dom_size.append(fn.row_vn.dom_size)
             end = len(x)
+            idx += 1
             self.fn_node_index[fn.name][j] = (start, end)
         self.x = torch.tensor(x, dtype=torch.float32, device=device)
+        self.scatter_indexes = torch.tensor(scatter_indexes, torch.long, device=device)
+        self.scatter_dom_size = torch.tensor(scatter_dom_size, device=device).unsqueeze(1)
 
         # build edge index
         edge_index = [[], []]
         src, dest = edge_index
         ass_to_sum_cnt = sum_to_ass_cnt = 0
         self.local_costs = []
+        # assignment node -> summarize node
         for fn in function_nodes:
             i = fn.row_vn.name
             j = fn.col_vn.name
@@ -80,7 +100,7 @@ class Feature:
                 # x_j -> f_ij
                 src += [idx for idx in range(*self.vn_node_index[j])]
                 dest += [self.fn_node_index[fn.name][j][0] + val] * len(fn.col_vn.dom_size)
-
+        # summarize node -> assignment node
         for fn in function_nodes:
             i = fn.row_vn.name
             j = fn.col_vn.name
@@ -104,6 +124,29 @@ class Feature:
         if sum_to_ass_prefix is not None:
             self.sum_to_ass_prefix = torch.tensor(sum_to_ass_prefix, dtype=torch.float32, device=device).repeat(sum_to_ass_cnt, 1)
         self.device = device
+        self.attentive_weights = dict()  # returned by neural network
+
+        for vn in variable_nodes:
+            in_idxes = []
+            out_indexes = []
+            for fn in vn.neighbors:
+                idx = self.function_nodes.index(fn)
+                idx *= 2
+                if vn == fn.row_vn:
+                    in_idxes.append(idx + 1)
+                    out_indexes.append(idx)
+                else:
+                    in_idxes.append(idx)
+                    out_indexes.append(idx + 1)
+                self.neighbor_idx_mapping[vn.name][fn.name] = out_indexes[-1]
+            for i in range(len(out_indexes)):
+                idxes = list(in_idxes)
+                idxes[i] = out_indexes[i]
+                self.neighbor_idx_info[vn.name][out_indexes[i]] = idxes
+
+        all_nodes = list(variable_nodes) + self.function_nodes
+        for node in all_nodes:
+            node.register_feature_extractor(self)
 
     def update_message(self):
         ass_to_sum = []
@@ -127,3 +170,59 @@ class Feature:
             msg = msg_to_tensor(i.income_msg[fn.name], self.device)
             sum_to_ass.append(msg)
         return torch.cat(ass_to_sum).unsqueeze(1), torch.cat(sum_to_ass).unsqueeze(1)
+
+
+class AttentiveVariableNode(VariableNode):
+    def __init__(self, name, dom_size):
+        super().__init__(name, dom_size)
+        self.feature_extractor = None
+        self.ordered_neighbors = []
+        self.device = ''
+        self.distribution = None
+
+    def register_feature_extractor(self, feature_extractor):
+        self.feature_extractor = feature_extractor
+        self.device = feature_extractor.device
+        for n in self.feature_extractor.function_nodes:
+            if n.name in self.neighbors:
+                self.ordered_neighbors.append(n.name)
+
+    def compute_msgs(self):
+        all_income_messages = [self.incoming_msg[n].detach() for n in self.ordered_neighbors]
+        for i, target in enumerate(self.ordered_neighbors):
+            msgs = list(all_income_messages)
+            msgs[i] = self.prev_sent[target] if target in self.prev_sent else torch.zeros(self.dom_size, dtype=torch.float32, device=self.device)
+            msgs = torch.stack(msgs, dim=1)
+            results = torch.mm(msgs, self.feature_extractor.attentive_weights[self.name][self.feature_extractor.neighbor_idx_mapping[self.name][target]])
+            results = results.mean(dim=1)
+            results = results - results.min().item()
+            self.prev_sent[target] = results.detach()
+            self.neighbors[target].incoming_msg[self.name] = results
+
+    def compute_belief(self):
+        all_income_messages = torch.stack([self.incoming_msg[n] for n in self.ordered_neighbors], dim=0)
+        belief = all_income_messages.sum(dim=0)
+        self.distribution = torch.softmax(belief, dim=0)
+
+    def compute_local_loss(self):
+        loss = 0
+        i_dist = self.distribution.unsqueeze(0)
+        for fn in self.neighbors.values():
+            if self == fn.row_vn:
+                j_dist = fn.col_vn.distribution.unsqueeze(1)
+                expected_cost = torch.mm(i_dist, fn.data)
+                expected_cost = torch.mm(expected_cost, j_dist)
+                loss = loss + expected_cost
+        return loss
+
+
+class AttentiveFunctionNode(FunctionNode):
+    def __init__(self, name, matirx, row_vn, col_vn):
+        super().__init__(name, matirx, row_vn, col_vn)
+        self.device = ''
+        self.data = None
+
+    def register_feature_extractor(self, feature_extractor):
+        self.device = feature_extractor.device
+        self.data = torch.tensor(self.matrix, dtype=torch.float32, device=self.device)
+
--- centralized/model.py
+++ centralized/model.py
@@ -1,13 +1,82 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from torch_geometric.nn import GATConv
+from torch_scatter import scatter_sum
+
+
+class Attention(nn.Module):
+    def __init__(self, hidden):
+        super().__init__()
+        self.query_proj = nn.Linear(hidden, hidden, bias=False)
+        self.key_proj = nn.Linear(hidden, hidden, bias=False)
+        self.score_proj = nn.Linear(2 * hidden, 1)
+
+    def forward(self, query, key):
+        query = self.query_proj(query)
+        key = self.key_proj(key)
+        return self.score_proj(torch.cat([query, key], dim=1))
 
 
 class AttentiveBP(nn.Module):
-    def __init__(self):
+    def __init__(self, in_channels, out_channels, ass_to_sum_inp_dim, ass_to_sum_hid_dim, sum_to_ass_inp_dim,
+                 sum_to_ass_hid_dim, num_heads=1):
         super().__init__()
-        
+        self.gru1 = nn.GRUCell(ass_to_sum_inp_dim, ass_to_sum_hid_dim)
+        self.gru2 = nn.GRUCell(sum_to_ass_inp_dim, sum_to_ass_hid_dim)
+        self.conv1 = GATConv(in_channels, 8, heads=4, edge_dim=sum_to_ass_hid_dim, concat=True)
+        self.conv2 = GATConv(32, 8, 4, edge_dim=sum_to_ass_hid_dim, concat=True)
+        self.conv3 = GATConv(32, 8, 4, edge_dim=sum_to_ass_hid_dim, concat=True)
+        self.conv4 = GATConv(32, out_channels, 4, edge_dim=sum_to_ass_hid_dim, concat=False)
+        self.attentions = nn.ModuleList()
+        for _ in range(num_heads):
+            self.attentions.append(Attention(out_channels))
 
     def forward(self, x, edge_index, ass_to_sum_prefix, local_costs, ass_to_sum_msg,
-                ass_to_sum_hidden, sum_to_ass_prefix, sum_to_ass_msg, sum_to_ass_hidden):
-        pass
+                ass_to_sum_hidden, sum_to_ass_prefix, sum_to_ass_msg, sum_to_ass_hidden,
+                scatter_indexes, scatter_dom_size,
+                neighbor_idx_info  # {x_i: {idx: []}}
+                ):
+        # update edge hidden
+        hidden1 = self.gru1(ass_to_sum_msg, ass_to_sum_hidden)
+        hidden2 = self.gru2(sum_to_ass_msg, sum_to_ass_hidden)
+
+        # construct edge feature
+        edge_attr1 = torch.cat([ass_to_sum_prefix, local_costs, hidden1], dim=1)
+        edge_attr2 = torch.cat([sum_to_ass_prefix, hidden2], dim=1)
+        assert edge_attr1.shape[1] == edge_attr2.shape[1]
+        assert edge_attr1.shape[0] + edge_attr2.shape[0] == edge_index.shape[1]
+        edge_attr = torch.cat([edge_attr1, edge_attr2], dim=0)
+
+        # graph conv
+        x = self.conv1(x, edge_index, edge_attr)
+        x = self.conv2(x, edge_index, edge_attr)
+        x = self.conv3(x, edge_index, edge_attr)
+        x = self.conv4(x, edge_index, edge_attr)
+
+        # pooling
+        pooling = scatter_sum(x, scatter_indexes, dim=0)[1:]  # each row is the sum of embeddings for a set of summarize node
+        pooling = pooling / scatter_dom_size
+
+        # attention score
+        num_msg_directions = pooling.shape[0]  # should be 2 times of # of factors
+        query = pooling.repeat_interleave(num_msg_directions, dim=0)
+        key = pooling.repeat(num_msg_directions, 1)
+        attention_scores = []
+        for m in self.attentions:
+            attention_scores.append(m(query, key))
+        attention_scores = torch.cat(attention_scores, dim=1)
+
+        # attention weights
+        attention_weights = dict()
+        for x_i, target in neighbor_idx_info.items():
+            attention_weights[x_i] = dict()
+            for idx in target.keys():
+                src = target[idx]
+                if type(src) is list:
+                    src = torch.tensor(src, dtype=torch.long, device=x.device, requires_grad=False)
+                src = src + idx * num_msg_directions
+                scores = attention_scores[src]
+                weights = F.softmax(scores, dim=0)
+                attention_weights[x_i][idx] = weights * (len(target) - 1)
+        return attention_weights, hidden1, hidden2
