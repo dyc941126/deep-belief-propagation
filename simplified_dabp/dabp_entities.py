lab - correct the weighting part, see simplified_dabp/model.py for details

simplified_dabp/dabp_entities.py
@@ -46,7 +46,7 @@ class AttentiveVariableNode(VariableNode):
     def compute_local_loss(self):
         normalized_dist = self.distribution + 1e-6
         entropy = -(normalized_dist * torch.log2(normalized_dist)).sum()
-        loss = 0.1 * entropy
+        loss = 0.0 * entropy
         i_dist = self.distribution.unsqueeze(0)
         for fn in self.neighbors.values():
             if self == fn.row_vn:
@@ -56,12 +56,22 @@ class AttentiveVariableNode(VariableNode):
                 loss = loss + expected_cost.squeeze()
         return loss
 
+    def reset(self):
+        for n in self.incoming_msg.keys():
+            if not type(self.incoming_msg[n]) is list:
+                self.incoming_msg[n] = self.incoming_msg[n].detach().clone()
+
 
 class AttentiveFunctionNode(FunctionNode):
     def __init__(self, name, matirx, row_vn, col_vn):
         super().__init__(name, matirx, row_vn, col_vn)
         self.device = ''
         self.data = None
+
+    def reset(self):
+        for n in self.incoming_msg.keys():
+            if not type(self.incoming_msg[n]) is list:
+                self.incoming_msg[n] = self.incoming_msg[n].detach().clone()
 
     def register_feature_extractor(self, feature_extractor):
         self.device = feature_extractor.device

simplified_dabp/model.py
@@ -67,10 +67,28 @@ class AttentiveBP(nn.Module):
             attention_weights[x_i] = dict()
             for idx in target.keys():
                 src = target[idx]
+                if len(src) == 1:
+                    attention_weights[x_i][idx] = torch.zeros(1, attention_scores.shape[1], dtype=torch.float32, device=x.device)
+                    continue
+                out_i = src.index(idx)
+
+                src = [ii for ii in src if ii != idx]
+                assert len(src) == len(target[idx]) - 1
+
                 if type(src) is list:
                     src = torch.tensor(src, dtype=torch.long, device=x.device, requires_grad=False)
                 src = src + idx * num_msg_directions
                 scores = attention_scores[src]
+                trg_score = attention_scores[idx + idx * num_msg_directions]
+                score_sum = torch.mean(scores, dim=0)
+                total_weight = F.softmax(torch.stack([score_sum, trg_score], dim=0), dim=0)
+
                 weights = F.softmax(scores, dim=0)
-                attention_weights[x_i][idx] = weights * (len(target) - 1)
+                weights = weights * total_weight[0].unsqueeze(0)
+                weights = weights * (len(target) - 1)
+
+                trg_weights = total_weight[1].unsqueeze(0)
+
+                weights = torch.cat([weights[: out_i], trg_weights, weights[out_i:]], dim=0)
+                attention_weights[x_i][idx] = weights
         return attention_weights, hidden1, hidden2